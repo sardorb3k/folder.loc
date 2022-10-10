cc9b447ca3d3b9f976e07643b9aef5ed71168ebc
Akmaljon Abdirakhimov <akmaljonabdirakhimov@gmail.com>
2022-10-10 12:54:13 +0500

    dashboard components order

--- resources/views/dashboard/admin.blade.php
+++ resources/views/dashboard/admin.blade.php
@@ -14,7 +14,7 @@
 
     <div class="nk-block">
         <div class="row g-gs">
-            <div class="col-lg-7 col-xxl-6">
+            <div class="col-lg-6 col-xxl-6">
                 <div class="card card-bordered h-100">
                     <div class="card-inner">
                         <div class="card-title-group pb-3 g-2">
@@ -65,49 +65,6 @@
                     </div>
                 </div><!-- .card -->
             </div><!-- .col -->
-            <div class="col-md-5 col-xxl-3">
-                <div class="card card-bordered h-100">
-                    <div class="card-inner mb-n2">
-                        <div class="card-title-group">
-                            <div class="card-title card-title-sm">
-                                <h6 class="title">Students who did not attend class</h6>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="card-inner">
-                        <table class="datatable-init nk-tb-list nk-tb-ulist no-footer" data-auto-responsive="false" id="DataTables_Table_1" aria-describedby="DataTables_Table_1_info">
-                            <thead>
-                                <tr class="nk-tb-item nk-tb-head">
-                                    <th class="nk-tb-col sorting" tabindex="0" aria-controls="DataTables_Table_1" rowspan="1" colspan="1">
-                                        <span class="sub-text">#</span>
-                                    </th>
-                                    <th class="nk-tb-col sorting" tabindex="0" aria-controls="DataTables_Table_1" rowspan="1" colspan="1">
-                                        <span class="sub-text">Student</span>
-                                    </th>
-                                    <th class="nk-tb-col tb-col-mb sorting" tabindex="0" aria-controls="DataTables_Table_1" rowspan="1" colspan="1">
-                                        <span class="sub-text">Missed days</span>
-                                    </th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($attendance_n as $attendance)
-                                    <tr class="nk-tb-item odd">
-                                        <td class="nk-tb-col nk-tb-col-check sorting_1">
-                                            <span>{{ $loop->iteration }}</span>
-                                        </td>
-                                        <td class="nk-tb-col">
-                                            <span class="text-capitalize">{{ $attendance->fullname }}</span>
-                                        </td>
-                                        <td class="nk-tb-col tb-col-lg">
-                                            <span>{{ $attendance->day }}</span>
-                                        </td>
-                                    </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-                    </div>
-                </div><!-- .card -->
-            </div>
             <div class="col-md-6 col-xxl-3">
                 <div class="card card-bordered h-100">
                     <div class="card-inner">
@@ -155,6 +112,49 @@
                                 @endforeach
                             </div><!-- .traffic-channel-group -->
                         </div><!-- .traffic-channel -->
+                    </div>
+                </div><!-- .card -->
+            </div>
+            <div class="col-md-6 col-xxl-3">
+                <div class="card card-bordered h-100">
+                    <div class="card-inner mb-n2">
+                        <div class="card-title-group">
+                            <div class="card-title card-title-sm">
+                                <h6 class="title">Students who did not attend class</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-inner">
+                        <table class="datatable-init nk-tb-list nk-tb-ulist no-footer" data-auto-responsive="false" id="DataTables_Table_1" aria-describedby="DataTables_Table_1_info">
+                            <thead>
+                                <tr class="nk-tb-item nk-tb-head">
+                                    <th class="nk-tb-col sorting" tabindex="0" aria-controls="DataTables_Table_1" rowspan="1" colspan="1">
+                                        <span class="sub-text">#</span>
+                                    </th>
+                                    <th class="nk-tb-col sorting" tabindex="0" aria-controls="DataTables_Table_1" rowspan="1" colspan="1">
+                                        <span class="sub-text">Student</span>
+                                    </th>
+                                    <th class="nk-tb-col tb-col-mb sorting" tabindex="0" aria-controls="DataTables_Table_1" rowspan="1" colspan="1">
+                                        <span class="sub-text">Missed days</span>
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($attendance_n as $attendance)
+                                    <tr class="nk-tb-item odd">
+                                        <td class="nk-tb-col nk-tb-col-check sorting_1">
+                                            <span>{{ $loop->iteration }}</span>
+                                        </td>
+                                        <td class="nk-tb-col">
+                                            <span class="text-capitalize">{{ $attendance->fullname }}</span>
+                                        </td>
+                                        <td class="nk-tb-col tb-col-lg">
+                                            <span>{{ $attendance->day }}</span>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
                     </div>
                 </div><!-- .card -->
             </div>
