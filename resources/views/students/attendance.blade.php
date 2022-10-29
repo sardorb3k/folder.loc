60e971e0f6af515bb42c109ae1d3d6e8d50ba523
Akmaljon Abdirakhimov <akmaljonabdirakhimov@gmail.com>
2022-10-29 08:05:01 +0500

    attendance calendar in students page

--- resources/views/students/attendance.blade.php
+++ resources/views/students/attendance.blade.php
@@ -16,12 +16,11 @@
                 <div class="nk-block-head">
                     <div class="nk-block-between">
                         <div class="nk-block-head-content">
-                            <h4 class="nk-block-title">Attendance</h4>
-                            <div class="nk-block-des">
-                                <p>You can count the days you are not in school<span class="text-soft"><em
-                                            class="icon ni ni-info"></em></span></p>
-                                <p style="border-left: 10px solid antiquewhite;"> &nbsp; Absent</p>
-                            </div>
+                            <h6 class="title">{{ __('dashboard.attendance') }}</h6>
+                            <p class="attendance-card-title">
+                                <span class="span-box-success"></span> {{ __('dashboard.present') }} &nbsp;
+                                <span class="span-box-danger"></span> {{ __('dashboard.absent') }}
+                            </p>
                         </div>
                         <div class="nk-block-head-content align-self-start d-lg-none">
                             <a href="#" class="toggle btn btn-icon btn-trigger mt-n1" data-target="userAside"><em
@@ -29,39 +28,63 @@
                         </div>
                     </div>
                 </div><!-- .nk-block-head -->
-                <table class="datatable-init-export nk-tb-list nk-tb-ulist no-footer" data-auto-responsive="false" id="DataTables_Table_1" aria-describedby="DataTables_Table_1_info">
-                    <thead>
-                        <tr class="nk-tb-item nk-tb-head">
-                            <th class="nk-tb-col sorting" tabindex="0" aria-controls="DataTables_Table_1" rowspan="1" colspan="1">
-                                <span class="sub-text">#</span>
-                            </th>
-                            <th class="nk-tb-col tb-col-mb sorting" tabindex="0" aria-controls="DataTables_Table_1" rowspan="1" colspan="1">
-                                <span class="sub-text">Group</span>
-                            </th>
-                            <th class="nk-tb-col tb-col-mb sorting" tabindex="0" aria-controls="DataTables_Table_1" rowspan="1" colspan="1">
-                                <span class="sub-text">Date</span>
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($attendance as $atten)
-                            <tr class="nk-tb-item odd" style="{{ $atten->mark == 0 ? 'background:antiquewhite' : '' }}">
-                                <td class="nk-tb-col nk-tb-col-check sorting_1">
-                                    <span>{{ $loop->iteration }}</span>
-                                </td>
-                                <td class="nk-tb-col tb-col-mb">
-                                    <span class="text-capitalize">
-                                        {{  $atten->level . ' ' . $atten->name }} 
-                                    </span>
-                                </td>
-                                <td class="nk-tb-col tb-col-lg">
-                                    <span>{{ $atten->attendance_date }}</span>
-                                </td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+                <div class="card-inner">
+                    <div id="attendanceCalendar">
+                    </div>
+                </div>
             </div><!-- .card-inner -->
         </div><!-- .card -->
     </div><!-- .nk-block -->
+
+    <script>
+         $(document).ready(function () {
+            // initialize calendar
+            const attendance = {!! json_encode($attendance) !!};
+            const events = [];
+            attendance.forEach(attend => {
+                events.push({
+                    id: 'attendance-' + attend.id,
+                    title: attend.mark == 1 ? "{{ __('dashboard.present') }}" : "{{ __('dashboard.absent') }}",
+                    start: attend.attendance_date,
+                    className: attend.mark == 1 ? 'fc-event-success' : 'fc-event-danger'
+                });
+            });
+
+            var calendarEl = document.getElementById('attendanceCalendar');
+            var calendar = new FullCalendar.Calendar(calendarEl, {
+                timeZone: 'UTC',
+                initialView: 'dayGridMonth',
+                themeSystem: 'bootstrap',
+                headerToolbar: {
+                    left: 'title prev,next',
+                    center: null,
+                    right: 'today'
+                },
+                firstDay: 1,
+                height: 520,
+                nowIndicator: true,
+                events: events,
+            });
+            calendar.render();
+
+            new AutoNumeric.multiple('.exam-result-input', {
+                decimalPlaces: 0,
+                minimumValue: 0,
+                maximumValue: 100,
+                watchExternalChanges: true
+            });
+
+            if($('.payment-amount').length > 0) {
+                new AutoNumeric('.payment-amount', {
+                    decimalPlaces: 0,
+                    minimumValue: 0
+                });
+            }
+            
+            $(".col-sm-4").on("input", function() {
+                $("#result").text(resultExam());
+            });
+            $('#result').text(resultExam());
+        });
+    </script>
 @endsection
