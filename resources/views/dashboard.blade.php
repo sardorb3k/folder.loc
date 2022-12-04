79b9585c61c3afefe7aed5e29aa1be3065496603
Akmaljon Abdirakhimov <akmaljonabdirakhimov@gmail.com>
2022-12-04 11:21:31 +0500

    random commit

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,7 +6,7 @@
             <div class="nk-block-head-content">
                 <h3 class="nk-block-title page-title">Website Analytics</h3>
                 <div class="nk-block-des text-soft">
-                    <p>Welcome to Analytics Dashboard.</p>
+                    <p>Welcome to Dashboard.</p>
                 </div>
             </div><!-- .nk-block-head-content -->
             <div class="nk-block-head-content">
