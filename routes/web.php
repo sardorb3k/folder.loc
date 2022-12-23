e27e2523433003283ffa58e9983ebc80855e5ae3
sardorb3k <s.sardorbek.s.802@gmail.com>
2022-12-23 20:54:22 +0500

    Menu change dashboard

--- routes/web.php
+++ routes/web.php
@@ -49,6 +49,7 @@ Route::group(['middleware' => ['web']], function () {
 
 // Home page route for logged in users only (middleware auth)
 Route::get('/', ['as' => 'dashboard', 'uses' => 'App\Http\Controllers\HomeController@index'])->middleware(['auth', 'roles:superadmin,admin,teacher,student']);
+Route::get('/dashboard', ['as' => 'dashboard', 'uses' => 'App\Http\Controllers\HomeController@index'])->middleware(['auth', 'roles:superadmin,admin,teacher,student']);
 
 // Group routes
 Route::group(['prefix' => 'groups'], function () {
