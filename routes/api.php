047294a1fdd737e85e06148d012999d3df295679
sardorb3k <s.sardorbek.s.802@gmail.com>
2023-01-06 15:00:35 +0500

    ..

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,6 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Mobile\AuthController;
-use Telegram;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -29,8 +27,4 @@ Route::group([
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::post('/refresh', [AuthController::class, 'refresh']);
     Route::get('/user-profile', [AuthController::class, 'userProfile']);
-});
-Route::post('/bot/getupdates', function() {
-    $updates = Telegram::getUpdates();
-    return (json_encode($updates));
 });
